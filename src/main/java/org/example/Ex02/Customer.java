e97fb7d140c0897646cab10d4e7285ed180b1e44
Maksim <maksim_belotelov@mail.ru>
2023-10-29 19:42:38 +0300

    home work

--- src/main/java/org/example/Ex02/Customer.java
+++ src/main/java/org/example/Ex02/Customer.java
@@ -1,14 +1,21 @@
 package org.example.Ex02;
 
 public class Customer {
+    enum Genders {
+        FEMALE,
+        MALE
+    };
+
     private String name;
     private String phoneNumber;
     private int age;
+    private Genders gender;
 
-    public Customer(String name, String phoneNumber, int age) {
+    public Customer(String name, String phoneNumber, int age, Genders gender) {
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.age = age;
+        this.gender = gender;
     }
 
     public String getName() {
@@ -23,12 +30,21 @@ public class Customer {
         return age;
     }
 
+    public Genders getGender() {
+        return gender;
+    }
+
+    public void setGender(Genders gender) {
+        this.gender = gender;
+    }
+
     @Override
     public String toString() {
         return "Customer{" +
                 "name='" + name + '\'' +
                 ", phoneNumber='" + phoneNumber + '\'' +
                 ", age=" + age +
+                ", gender=" + gender +
                 '}';
     }
 }
--- src/main/java/org/example/Ex02/Main.java
+++ src/main/java/org/example/Ex02/Main.java
@@ -1,8 +1,19 @@
 package org.example.Ex02;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Main {
-    private static Customer[] customers = {new Customer("Max", "+79998887766", 20),
-            new Customer("Sam", "+79999998877", 25)};
+    enum Holidays {
+       WEEKDAY,
+       NEWYEAR,
+       INTWOMENSDAY,
+       MANSDAY
+    };
+
+    private static Customer[] customers = {new Customer("Max", "+79998887766",
+                                20, Customer.Genders.MALE),
+            new Customer("Sam", "+79999998877", 25, Customer.Genders.FEMALE)};
 
     private static Product[] products = {new Product("Вода", 40), new Product("Шоколад", 55),
             new Product("Хлеб", 45), new Product("Молоко", 75),
@@ -17,8 +28,10 @@ public class Main {
                                 new Order(customers[1], products[2], 2),
                                 new Order(customers[0], new Product("Кашка", 25), 3), // Нет товара
                                 new Order(customers[1], products[1], -2), // Некорректное количество
-                                new Order(new Customer("Vasiliy", "+79876543214", 30),
-                                        products[0], 1) };
+                                new Order(new Customer("Vasiliy", "+79876543214", 30,
+                                        Customer.Genders.MALE), products[0], 1) }; // Некорректный пользователь
+
+        congratulate(customers);
 
         for (Order currentOrder : testOrdersData) {
             try {
@@ -73,5 +86,38 @@ public class Main {
 
     static class AmountException extends RuntimeException {
         AmountException(String message) { super(message); }
+    }
+
+    public static void congratulate(Customer[] customers) {
+        Calendar currentDate = new GregorianCalendar();
+        Holidays holidayToday = Holidays.WEEKDAY;
+
+        if (currentDate.get(GregorianCalendar.MONTH) == Calendar.DECEMBER
+                && currentDate.get(GregorianCalendar.DAY_OF_MONTH) == 31)
+            holidayToday = Holidays.NEWYEAR;
+        else if ((currentDate.get(GregorianCalendar.MONTH) == Calendar.MARCH)
+                && currentDate.get(GregorianCalendar.DAY_OF_MONTH) == 8)
+            holidayToday = Holidays.INTWOMENSDAY;
+        else if ((currentDate.get(GregorianCalendar.MONTH) == Calendar.FEBRUARY)
+                && currentDate.get(GregorianCalendar.DAY_OF_MONTH) == 23)
+            holidayToday = Holidays.MANSDAY;
+
+        if(!holidayToday.equals(Holidays.WEEKDAY)) {
+            for (Customer customer : customers) {
+                switch(holidayToday) {
+                    case NEWYEAR:
+                        System.out.println("С Новым годом, " + customer.getName());
+                        break;
+                    case INTWOMENSDAY:
+                        if(customer.getGender().equals(Customer.Genders.FEMALE))
+                            System.out.println("С 8 Марта, " + customer.getName());
+                        break;
+                    case MANSDAY:
+                        if(customer.getGender().equals(Customer.Genders.MALE))
+                            System.out.println("С 23 Февраля, " + customer.getName());
+                        break;
+                }
+            }
+        }
     }
 }
